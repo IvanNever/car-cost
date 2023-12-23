48c259d7d9c69c0c6a5358f6f875a4d803da9b31
Ivan Neverovsky <ivan@neverovsky.com.ua>
2023-12-23 15:10:41 +0200

    managed app configuration

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -30,4 +30,24 @@ describe('Auth system', () => {
         expect(email).toEqual(baseEmail);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asder@mail.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'dsfdfsdfsfsdfsdf',
+      })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/current')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
