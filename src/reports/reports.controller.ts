ee12bd6b1cbf253003bccdd7b435bd70280be26e
Ivan Neverovsky <ivan@neverovsky.com.ua>
2023-12-27 19:55:33 +0200

    added entity tables relations

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Get()
+  findAllReports() {}
+
+  @Get()
+  findReport() {}
+
+  @Put()
+  @UseGuards(AuthGuard)
+  updateReport() {}
+
+  @Delete()
+  @UseGuards(AuthGuard)
+  deleteReport() {}
+}
